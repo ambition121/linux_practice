cc30acda396fb0f5fb74559e4fe8c4776dfc9a89
linux <zhang0502xin@sina.com>
2015-03-26 15:27:42 +0800

    处理ls使之宽度尽量等宽

---
+++ chapter3/ls3_3-10.c
@@ -0,0 +1,175 @@
+#define	SCREEN_WIDTH	80  /* for now */
+
+#include    <stdio.h>
+#include    <stdlib.h>
+#include    <sys/types.h>
+#include    <dirent.h>
+
+void do_ls(char []);
+
+main(int ac, char *av[])
+{
+    if ( ac == 1 )
+	do_ls( "." );
+    else
+	while ( --ac ){
+	    printf("%s:\n", *++av );
+	    do_ls( *av );
+	}
+}
+
+void do_ls( char dirname[] )
+    /*
+     *  list files in directory called dirname
+     */
+{
+    DIR	*dir_ptr;	/* the directory */
+    struct dirent   *direntp;	    /* each entry    */
+
+    list_init();
+
+    if ( ( dir_ptr = opendir( dirname ) ) == NULL )
+	fatal("cannot open ", dirname);
+    else
+    {
+	while ( ( direntp = readdir( dir_ptr ) ) != NULL )
+	    list_append( direntp->d_name );
+	closedir(dir_ptr);
+    }
+
+    list_show();
+    list_finish();
+}
+
+/*
+ * list functions here
+ *  a list of names is an array that grows as needed
+ */
+
+void *emalloc(unsigned), *erealloc(void*, unsigned);
+char **namelist = NULL;
+int  listlen = 0;
+int  taken = 0;	    //名字的个数
+
+#define	CHUNKLEN  100
+
+list_init()
+{
+    namelist = (char **) emalloc(CHUNKLEN * sizeof(char *));
+    listlen = CHUNKLEN;
+    taken = 0;
+}
+
+/*
+ * add an item to the list, extending it if needed
+ */
+list_append(char *str)
+{
+    char	*newstring(char *);
+    char    *name = newstring(str);
+
+    if ( taken == listlen ){
+	listlen += CHUNKLEN;
+	namelist = (char **) erealloc(namelist,listlen);
+    }
+    namelist[taken++] = name ;
+}
+
+/*
+ * figure out max length, then figure out number of cols we
+ * can do and then list them
+ */
+list_show()
+{
+    int	i;
+    int maxlen = 0;
+    int	pagewidth;
+    int cols, col_len, col_wid;
+    int	row, col;
+    char	*name;
+
+    /* figure out lengths and widths of columns */
+    for(i=0; i<taken; i++)
+	if ( strlen(namelist[i]) > maxlen )
+	    maxlen = strlen(namelist[i]);
+
+    col_wid = maxlen+1;		    //列宽等于名字最长名字加1
+    pagewidth = get_screen_width(); //页宽是定义到宏  这里等于80
+    cols = pagewidth / col_wid ;    //列个数等于屏幕宽除以页宽
+    col_len = taken / cols ;	    //列长等于名字个数除以列个数
+    if ( taken % cols )	/* for uneven cols, put the */
+	col_len++;	    /* extra on the left        */
+
+    /* print the list using these numbers */
+
+    for( row = 0 ; row < col_len ; row++ )
+	for( col = 0 ; col < cols ; col++ ){
+	    i = row + col*col_len ;
+	    if ( i < taken ){
+		if ( i + col_len < taken )
+		    printf("%-*s",col_wid,namelist[i]);
+		else
+		    printf("%s\n",namelist[i]);
+	    }
+	}
+}
+
+/*
+ * delete the words and the list
+ */
+list_finish()
+{
+    int	i;
+
+    for(i = 0 ; i<taken ; i++ )
+	free(namelist[i]);
+    free(namelist);
+    namelist = NULL;
+    taken = listlen = 0 ;
+}
+
+/*
+ * in the future, we can ask the system for the width of
+ * the screen
+ */
+    int
+get_screen_width()
+{
+    return SCREEN_WIDTH;
+}
+/*
+ * construct a string 
+ */
+char *newstring(char *s)
+{
+    char    *rv = emalloc( 1 + strlen(s) );
+    strcpy(rv, s);
+    return rv;
+}
+
+    void *
+emalloc(unsigned n)
+{
+    void *rv = malloc(n);
+    if ( rv )
+	return rv;
+    fatal("out of memory","");
+}
+
+    void *
+erealloc(void *p, unsigned n)
+{
+    void *rv = realloc(p,n);
+    if ( rv )
+	return rv;
+    fatal("out of memory","");
+}
+/*
+ * report and die
+ */
+
+fatal(char *s1, char *s2)
+{
+    fprintf(stderr,"ls1: %s%s\n", s1, s2);
+    exit(1);
+}
