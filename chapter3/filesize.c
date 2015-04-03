2f50258d40bea47b9ad8f2b7d633ef5a48df8000
linux <zhang0502xin@sina.com>
2015-04-03 18:19:46 +0800

    fopen这个函数的小例子

---
+++ chapter3/filesize.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<sys/stat.h>
+int main()
+{
+    struct stat infobuf;
+    if(stat("/etc/passwd",&infobuf) == -1)
+	perror("/etc/passwd");
+
+    else
+	printf("the size of /etc/passwd is %d\n",infobuf.st_size);
+    }
