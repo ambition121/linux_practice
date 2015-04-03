f5256714ac52b6e4bb236bad661322f232d18cb6
linux <zhang0502xin@sina.com>
2015-04-03 18:06:12 +0800

    who命令的一个程序

---
+++ chapter2/who1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<utmp.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+#define SHOWHOST
+
+int main()
+{
+	struct utmp current_record;
+	int utmpfd;
+	int reclen = sizeof(current_record);
+	if((utmpfd = open(UTMP_FILE,O_RDONLY)) == -1){
+		perror(UTMP_FILE);
+		exit(1);
+		}
+	while(read(utmpfd,&current_record,reclen) == reclen)
+		show_info(&current_record);
+	close(utmpfd);
+		return 0;
+	}
+show_info(struct utmp * utbufp)
+{
+	 if(utbufp->ut_type !=USER_PROCESS)
+	   return;
+	printf("% -8.8s",utbufp->ut_name);
+	printf(" ");
+	printf("% -8.8s",utbufp->ut_line);
+	printf(" ");
+	printf("%10ld",utbufp->ut_time);
+	printf(" ");
+	printf("(%s)",utbufp->ut_host);
+	printf("\n");
+	}
